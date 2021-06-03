3688d5ee7492c2aa66dee33ebffe20ad942a1337
Yevgeniia Motytskaya <qayevheniia@gmail.com>
2021-06-03 20:52:36 +0300

    Did with class scanner

--- src/com/mysite/HW_02.java
+++ src/com/mysite/HW_02.java
@@ -1,9 +1,10 @@
 package com.mysite;
+import java.util.Scanner;
 
 public class HW_02 {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
 
 /*1. Write a Java program that reads a number in inches, converts it to meters.
                 Note: One inch is 0.0254 meter.
@@ -13,8 +14,9 @@ public class HW_02 {
         1000.0 inch is 25.4 meters
  */
 
-	float value_inh = 1000.0f;
-	float inh_meter = 0.0254f;
+        System.out.print("Input a value for inch: ");
+        float value_inh = sc.nextFloat();
+        float inh_meter = 0.0254f;
         System.out.println(value_inh + " inch is "  + value_inh*inh_meter +" meters");
 
 /*2. Write a Java program to convert minutes into a number of years and days.
@@ -23,11 +25,11 @@ Input the number of minutes: 3456789
 Expected Output :
 3456789 minutes is approximately 6 years and 210 days
 */
-
-    int value_in_minutes = 3456789;
-    int in_days = value_in_minutes/(60*24);
-    int in_year = in_days/365;
-    int years_days = in_days%365;
+        System.out.print("\nInput the number of minutes: ");
+        int value_in_minutes = sc.nextInt();
+        int in_days = value_in_minutes/(60*24);
+        int in_year = in_days/365;
+        int years_days = in_days%365;
 
     System.out.println(value_in_minutes +" minutes is approximately "
             +in_year + " years" +" and " +years_days + " days");
@@ -39,11 +41,13 @@ Input height in inches: 72
 Expected Output:
 Body Mass Index is 61.30159143458721*/
 
-    int weight_pounds = 452;
-    int height_in_inh = 72;
-    double pound_in_kg = 0.4535923745;
-    double inh_in_meters = 0.0254;
-    int sqr=2;
+        System.out.print("\nInput weight in pounds: ");
+        double weight_pounds = sc.nextDouble();
+        System.out.print("Input height in inches: ");
+        double height_in_inh = sc.nextDouble();
+        double pound_in_kg = 0.4535923745;
+        double inh_in_meters = 0.0254;
+        int sqr=2;
         System.out.println("Body Mass Index is " +(weight_pounds*pound_in_kg)/((Math.pow(height_in_inh*inh_in_meters,sqr))));
 
 /*4. Write a Java program to takes the user for a distance (in meters) and the time was taken (as three numbers: hours, minutes, seconds), and display the speed, in meters per second, kilometers per hour
@@ -57,12 +61,17 @@ Expected Output :
 Your speed in meters/second is 0.11691531
 Your speed in km/h is 0.42089513*/
 
-    float distance = 2500;
-    float hours = 5;
-    float minutes = 56;
-    float seconds = 23;
-    float in_min_sec = 60;
-    float meters_km = 1000;
+
+        System.out.print("\nInput distance in meters: ");
+        float distance = sc.nextFloat();
+        System.out.print("Input hour: ");
+        float hours = sc.nextFloat();
+        System.out.print("Input minutes: ");
+        float minutes = sc.nextFloat();
+        System.out.print("Input seconds: ");
+        float seconds = sc.nextFloat();
+        float in_min_sec = 60;
+        float meters_km = 1000;
 
         System.out.println("Your speed in meters/second is " +(distance/((hours*in_min_sec*in_min_sec)+(minutes*in_min_sec)+seconds)));
         System.out.println("Your speed in km/h is " +((distance/meters_km)/(hours+(minutes/in_min_sec)+
@@ -71,13 +80,11 @@ Your speed in km/h is 0.42089513*/
 
 //5. Write a Java program to generate random integers in a specific range.(Hint: Use Math class)
 
-
-        int minimum = 10;
-        int maximum =150;
-
+        System.out.print("\nInput minimum number: ");
+        int minimum = sc.nextInt();
+        System.out.print("Input maximum  number: ");
+        int maximum = sc.nextInt();
         System.out.println("random integer = " + ((minimum + (int)(Math.random() * (maximum-minimum)))));
-//I don't understand Do we need to include specific max and minimum numbers in range?
-// So I did that max number isn't included but min is included in
 
     }
 }
